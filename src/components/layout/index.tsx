6dd794e748d3345e270d462cc02752be12f34875
Jinho Hyeon <neverlish@gmail.com>
2019-09-21 13:11:24 +0900

    default seo

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,21 +1,28 @@
 import { graphql, Link, StaticQuery } from 'gatsby';
 import * as React from 'react';
+import { Helmet } from 'react-helmet';
 import { GetSiteMetadataQuery } from '../../../codegen/types';
 
 export default ({ children }: { children: JSX.Element | Array<JSX.Element[] | JSX.Element | string> }) => {
   return (
     <StaticQuery
       query={query}
-      render={(data: GetSiteMetadataQuery) => (
-        <main>
-          <header style={{ display: 'flex' }}>
-            <Link to='/'>{data.site!.siteMetadata!.title}</Link>
-            &nbsp;&nbsp;
+      render={(data: GetSiteMetadataQuery) => {
+        const { title } = data.site!.siteMetadata!;
+        return <>
+          <Helmet title={title!}>
+
+          </Helmet>
+          <main>
+            <header style={{ display: 'flex' }}>
+              <Link to='/'>{title}</Link>
+              &nbsp;&nbsp;
             <a href='https://github.com/neverlish/neverlish.me' target='_blank'>github</a>
-          </header>
-          {children}
-        </main>
-      )}
+            </header>
+            {children}
+          </main>
+        </>;
+      }}
     />
   );
 };
