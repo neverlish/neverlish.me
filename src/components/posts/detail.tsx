9eb410d3cfa3080e3b6664c443554b3e536bfb81
Jinho Hyeon <neverlish@gmail.com>
2019-09-21 11:49:23 +0900

    generate graphql schema types

--- src/components/posts/detail.tsx
+++ src/components/posts/detail.tsx
@@ -1,8 +1,20 @@
-import * as React from 'react'
-import { graphql } from 'gatsby'
+import { graphql } from 'gatsby';
+import * as React from 'react';
+import { PostDetailQuery } from '../../../codegen/types';
+
+export default ({ data: { markdownRemark: post } }: { data: PostDetailQuery }) => {
+  return (
+    <div>
+      <div>
+        <h1>{post!.frontmatter!.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post!.html! }} />
+      </div>
+    </div>
+  )
+}
 
 export const query = graphql`
-  query($link: String!) {
+  query PostDetail ($link: String!) {
     markdownRemark(fields: { link: { eq: $link } }) {
       html
       frontmatter {
@@ -12,22 +24,3 @@ export const query = graphql`
   }
 `
 
-interface DetailProps {
-  data: {
-    markdownRemark: {
-      frontmatter: { title: string },
-      html: string
-    }
-  }
-}
-
-export default ({ data: { markdownRemark: post } }: DetailProps) => {
-  return (
-    <div>
-      <div>
-        <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
-    </div>
-  )
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
 import * as React from 'react'
 import { graphql, Link } from 'gatsby';
+import { IndexPageQuery } from '../../codegen/types';
+
+export default ({ data }: { data: IndexPageQuery }) => {
+  return (
+    <div>
+      {data.site!.siteMetadata!.title}
+      <h4>{data.allMarkdownRemark!.totalCount} Posts</h4>
+      {data.allMarkdownRemark!.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link to={node.fields!.link!}>
+            <h3>
+              {node.frontmatter!.title} — {node.frontmatter!.date}
+            </h3>
+          </Link>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export const query = graphql`
-  query {
+  query IndexPage {
     allMarkdownRemark {
       totalCount
       edges {
@@ -27,47 +47,3 @@ export const query = graphql`
   }
 `
 
-interface IndexProps {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string
-      }
-    }
-    allMarkdownRemark: {
-      totalCount: number
-      edges: Array<{
-        node: {
-          frontmatter: {
-            title: string;
-            date: string;
-          }
-          id: string;
-          fields: {
-            link: string;
-          }
-          excerpt: string;
-        }
-      }>
-    }
-  }
-}
-
-export default ({ data }: IndexProps) => {
-  return (
-    <div>
-      {data.site.siteMetadata.title}
-      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <Link to={node.fields.link}>
-            <h3>
-              {node.frontmatter.title} — {node.frontmatter.date}
-            </h3>
-          </Link>
-          <p>{node.excerpt}</p>
-        </div>
-      ))}
-    </div>
-  )
-}
